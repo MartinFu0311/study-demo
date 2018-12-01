235363a2c6ab3835b409bd03eec2f6a4a97dfe04
Martin <Martin@Martin-PC>
2018-12-01 22:28:52 +0800

    add comment for init Spring Security, with error of "No bean named 'springSecurityFilterChain' available".

--- web-demo/src/main/java/demo/martin/config/WebSecurityConfigure.java
+++ web-demo/src/main/java/demo/martin/config/WebSecurityConfigure.java
@@ -5,8 +5,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/*
+ * 曾经因为这里错误用了注解 @EnableWebMvc 而不是 @EnableWebSecurity 导致 Spring Security 不能启动成功，
+ * 报“No bean named 'springSecurityFilterChain' available”错误
+ * 对于因为有两个 WebApplicationInitializer ，一个是定义的继承自 AbstractAnnotationConfigDispatcherServletInitializer 的 DemoConfig，一个是
+ * 继承自 AbstractSecurityWebApplicationInitializer 的 SecurityApplicationInitializer，这两个的执行顺序究竟是否有影响还未知，后续可以再验证 @Order 是否有
+ * 作用，现在用的默认顺序，由 Spring 决定
+ */
 @EnableWebSecurity
 public class WebSecurityConfigure implements WebMvcConfigurer {
 
